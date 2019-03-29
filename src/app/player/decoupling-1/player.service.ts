cde4e6522f5e6303c423a43dd46ccb67d9f6b908
Albert Parrón <al.parron@gmail.com>
2019-03-29 03:55:05 +0100

    example

--- src/app/player/decoupling-1/player.service.ts
+++ src/app/player/decoupling-1/player.service.ts
@@ -1,16 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Player } from '../entities/index';
-import { map } from 'rxjs/operators';
+
 import { PlayerHttpService } from './player-http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerFacadeService {
-  private players$ = new BehaviorSubject([]);
+  private players$ = new BehaviorSubject({});
   private loading$ = new BehaviorSubject(false);
 
   constructor(private playerHttpService: PlayerHttpService) {
@@ -19,7 +19,7 @@ export class PlayerFacadeService {
   public loadAll() {
     this.loading$.next(true);
     return this.playerHttpService.loadAll()
-      .pipe(map(r => r))
+      .pipe(map(convertToObjectLiteral))
       .subscribe(r => {
         this.players$.next(r);
         this.loading$.next(false);
@@ -27,10 +27,24 @@ export class PlayerFacadeService {
   }
 
   public getAll(): Observable<Player[]> {
-    return this.players$;
+    return this.players$.pipe(map(convertToArray));
   }
 
   public getLoading(): Observable<boolean> {
     return this.loading$;
   }
 }
+
+// -------------------
+
+const convertToObjectLiteral = <T extends { id: string; }>(data: T[]): { [key: string]: T } => {
+  return data.reduce((acc, val) => {
+    return {...acc, [val.id]: val};
+  }, {});
+};
+
+const convertToArray = (data: { [key: string]: Player }): Player[] => {
+  return Object.keys(data).map(key => {
+    return data[key];
+  });
+};
